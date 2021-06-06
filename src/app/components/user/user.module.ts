174626685e11e72e6786d5dbdfe042944b436104
MoinShaikh611 <imoin611@gmail.com>
2021-06-06 17:56:56 +0530

    somthing

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -6,21 +6,18 @@ import { UserComponent } from './user.component';
 import { RegisterComponent } from './screens/register/register.component';
 import { FormsModule } from '@angular/forms';
 import { UserDetailComponent } from './screens/user-detail/user-detail.component';
-import { MessagesComponent } from '../messages/messages.component';
-
 
 @NgModule({
   declarations: [
     UserComponent,
     RegisterComponent,
     UserDetailComponent,
-    MessagesComponent
+    
   ],
   imports: [
     CommonModule,
     UserRoutingModule,
     FormsModule
-
-  ]
+  ],
 })
 export class UserModule { }
